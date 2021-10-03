8e289f52222674682f9daf254d765d401278f3ca
Thomas Diroll <hello@thomasdiroll.com>
2021-10-03 14:33:03 +0200

    Updates

---
+++ static/1633264382/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fintersolarnft.github.io\u002Fwebsite\u002Fstatic\u002F1633264382",layout:"default",error:null,serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fintersolarnft.github.io\u002Fwebsite\u002F"}}}}("\u002F"));
