09778626d7407d0ad5436e1d8146ad453dc42dc5
Thomas Diroll <hello@thomasdiroll.com>
2021-10-03 14:25:47 +0200

    Updates

--- website/_nuxt/static/1633263854/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fwebsite\u002F_nuxt\u002Fstatic\u002F1633263854",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fwebsite\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
